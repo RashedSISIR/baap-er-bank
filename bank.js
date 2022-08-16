dda2cf1b7faca973af72f985de1229674287b4d5
RashedSISIR <rashedsisir00@gmail.com>
2022-08-17 03:17:23 +0800

    added withdraw

--- bank.js
+++ bank.js
@@ -23,3 +23,24 @@ ballanceValue.innerText=newBallance
 
 
 })
+//withdraw//
+document.getElementById('button-field').addEventListener('click',function(){
+   const withdrawField=document.getElementById('input-field');
+   const newWithdrawString=withdrawField.value;
+   const newWithdrawAmmount=parseFloat(newWithdrawString);
+   
+   const withdrawInput=document.getElementById('withdraw-total');
+   const withdrawValue=withdrawInput.innerText;
+   const withdrawString=parseFloat(withdrawValue);
+
+   const totalWithdraw=newWithdrawAmmount+withdrawString;
+   withdrawInput.innerText=totalWithdraw;
+  
+   const ballanceField=document.getElementById('ballance-field');
+   const ballanceValue=ballanceField.innerText;
+   const ballancestring=parseFloat(ballanceValue);
+   const totalWithdrawAmmount=ballancestring-totalWithdraw;
+   ballanceField.innerText=totalWithdrawAmmount;
+   withdrawField.value='';
+}
+)
